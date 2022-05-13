d1daa4e2a7d86a8e28b07fd336a6c1b660b32768
Eskaine <eskaineliu@gmail.com>
2022-05-14 00:49:22 +0800

    added solidity linting

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,9 @@ class Navbar extends Component {
   render() {
     return (
       <div>
-        <div className="container mx-auto flex justify-between">
-              <span>NFT Marketplace</span>
-              <button>Login with Metamask</button>
+        <div className='container mx-auto flex justify-between'>
+              <span className='font-bold text-white'>NFT Marketplace</span>
+              <button className='rounded-full px-4 py-2 bg-blue-600 text-white'>Login with Metamask</button>
           </div>
       </div>
     )
