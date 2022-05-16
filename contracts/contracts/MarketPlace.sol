cc9b4342e85720746315b439d675054447a181f3
Eskaine <eskaineliu@gmail.com>
2022-05-16 16:47:48 +0800

    update marketplace.sol

--- contracts/contracts/MarketPlace.sol
+++ contracts/contracts/MarketPlace.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.0;
 import "../node_modules/hardhat/console.sol";
 
 contract MarketPlace {
-    constructor() {}
+    constructor() {
+        // what should be in here?
+    }
 
     function addNFT() {
+        // what params is pass in? check client
+        // the data is store in this contract, how is the data store?
+        // what are the checks?
 
+        // To push to blockchain, finish the above and we can proceed with this next
     }
 }
