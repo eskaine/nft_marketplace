ac9e29905d84e0b5f4c63f920be41f3b593cca67
Eskaine <eskaineliu@gmail.com>
2022-05-16 16:39:33 +0800

    added marketplace.sol

---
+++ contracts/contracts/MarketPlace.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "../node_modules/hardhat/console.sol";
+
+contract MarketPlace {
+    constructor() {}
+
+    function addNFT() {
+
+    }
+}
