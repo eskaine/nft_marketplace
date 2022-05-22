f140356ae1a4f279d0a6b7e88d8d371728083824
joycetml <joycetml29@gmail.com>
2022-05-22 23:34:34 +0800

    update on some of the functions based on feedback during zoom

--- contracts/contracts/MarketPlace.sol
+++ contracts/contracts/MarketPlace.sol
@@ -16,18 +16,19 @@ contract MarketPlace {
     //address payable public immutable feeAccount; // the account that receives fees
     struct NFT{
         uint256 id;
-        string label;
+        string name;
         address currentOwner;
         uint256 price;
+        string tokenurl;
         bool isListed;
     }
 
     mapping(uint256 => NFT) private items;
-    mapping(address => NFT[] ) private itemsbyaddress;
+    mapping(address => uint256[]) private itemsbyaddress; //joyce and zach: change to int array
  
     uint256[] items_listed; 
 
-    function addNFT(string memory label, uint256 price) public notContractOwner {
+    function addNFT(uint256 price, string memory name, string memory tokenUrl) public notContractOwner {
         totalNFTs++;
         uint256 newItemId = totalNFTs;
         //_mint(owner, newItemId);
@@ -35,29 +36,18 @@ contract MarketPlace {
         //create token here?
         
         NFT memory newNFT = NFT(
-            totalNFTs, label, msg.sender, price, false
+            totalNFTs, name, msg.sender, price, tokenUrl, false
         );
         
         items[newItemId] = newNFT;
         //add to the itemsbyaddress
-        itemsbyaddress[msg.sender].push(newNFT);
+        itemsbyaddress[msg.sender].push(newItemId);
        
 
     }
 
-    function removeFromList( uint256 id, address user ) private {
-        NFT[] storage itemslist = itemsbyaddress[user];
-        for ( uint256 i = 0; i < itemslist.length; i++ ){
-            if ( itemslist[i].id == id ) {
-                if ( itemslist.length > 1 ) {
-                    itemslist[i] = itemslist[itemslist.length-1]; //replace with last item in the list? hmm 
-                    //didnt want to have gap between them, any other idea how to do it? delete itemslist[i]?
-                }
-                itemslist.pop();              
-                break;
-            }
-        }
-        itemsbyaddress[user] = itemslist;
+    function removeFromList( uint256 id, address user ) private onlyNFTOwner(id) {
+        delete itemsbyaddress[user][id]; //joyce and zach: remove from list
     }
 
     function buyNFT( uint256 id ) public payable notContractOwner {
@@ -75,7 +65,7 @@ contract MarketPlace {
         removeFromList(id, items[id].currentOwner);
 
         items[id].currentOwner = msg.sender;//update to new owner of the nft
-        itemsbyaddress[msg.sender].push( items[id] );
+        itemsbyaddress[msg.sender].push( id );
     }
     
     //To list the NFT on the marketplace
@@ -97,9 +87,10 @@ contract MarketPlace {
     // return entire nft list, forget to mention listed nfts only
     function getNFTList() public view returns ( NFT[] memory ) {
         // instead of doing all these work to process the data, it's better to just store the id in the struct on creation
-        // how to return mapping without using for loop?
+        // joyce and zach: how to return mapping without using for loop?  
         NFT[] memory nfts = new NFT[](totalNFTs+1);
         uint count; 
+
         for ( uint i=0; i < totalNFTs+1; i++)
         {
             NFT storage nft = items[i];
@@ -111,9 +102,18 @@ contract MarketPlace {
         return (nfts);
     }
 
-    function getUserNFTList() public view returns (NFT[] memory) {
+    //joyce and zach: updated on 22 may 2022
+    function getUserNFTList(address user) public view returns (NFT[] memory) {
         // return the actual nft data not the id
-        return itemsbyaddress[msg.sender];
+        uint256[] memory useritems = itemsbyaddress[user];
+        NFT[] memory nfts;// = new NFT[](totalNFTs+1);
+        
+        for ( uint i=0; i < useritems.length; i++)
+        {
+            NFT storage nft = items[i];
+            nfts[i] = nft;
+        }
+        return (nfts);
     }
 
     modifier notContractOwner() {
