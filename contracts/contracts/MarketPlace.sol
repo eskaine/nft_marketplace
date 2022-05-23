3782262e5acbc10e4b588b3a29e309e9578a9485
Eskaine <eskaineliu@gmail.com>
2022-05-23 21:39:36 +0800

    Smart contracts creation (#5)
    * Update Branch (#4)
    
    * Smart contracts creation (#1)
    
    * add TheNFT.sol
    
    * update solidity version
    
    * added marketplace.sol
    
    * update marketplace.sol
    
    * add TheNFT.sol
    
    * update solidity version
    
    * added marketplace.sol
    
    * update marketplace.sol
    
    * add TheNFT.sol
    
    * update solidity version
    
    * added marketplace.sol
    
    * update marketplace.sol
    
    * Started on the basic code for marketplace
    
    * update on marketplace.sol
    
    * added comments
    
    * create market place as token
    
    * Added list and delist
    
    * added comments
    
    * update on functions
    
    Co-authored-by: joycetml <joycetml29@gmail.com>
    Co-authored-by: zacharyl <zacharyl@globalpsa.com>
    
    * Eskaine x (#2)
    
    * add TheNFT.sol
    
    * update solidity version
    
    * added marketplace.sol
    
    * update marketplace.sol
    
    * add TheNFT.sol
    
    * update solidity version
    
    * added marketplace.sol
    
    * update marketplace.sol
    
    * add TheNFT.sol
    
    * update solidity version
    
    * added marketplace.sol
    
    * update marketplace.sol
    
    * Started on the basic code for marketplace
    
    * update on marketplace.sol
    
    * added comments
    
    * create market place as token
    
    * Added list and delist
    
    * added comments
    
    * update on functions
    
    * added modifer and new comments
    
    * updated frontend
    
    * updated frontend
    
    Co-authored-by: joycetml <joycetml29@gmail.com>
    Co-authored-by: zacharyl <zacharyl@globalpsa.com>
    
    * added abi file
    
    * added ipfs
    
    Co-authored-by: joycetml <joycetml29@gmail.com>
    Co-authored-by: zacharyl <zacharyl@globalpsa.com>
    
    * setup local deployment
    
    * update on some of the functions based on feedback during zoom
    
    * add size for nft array
    
    * code cleanup
    
    Co-authored-by: joycetml <joycetml29@gmail.com>
    Co-authored-by: zacharyl <zacharyl@globalpsa.com>
    Co-authored-by: ries <faladonliu@gmail.com>

--- contracts/contracts/MarketPlace.sol
+++ contracts/contracts/MarketPlace.sol
@@ -4,116 +4,78 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MarketPlace {
-
     address public owner;
-    uint256 totalNFTs;
+    uint256 private totalNFTs;
 
     constructor() {
         owner = msg.sender;
     }
     
-    // Variables
-    //address payable public immutable feeAccount; // the account that receives fees
     struct NFT{
         uint256 id;
-        string label;
+        string name;
         address currentOwner;
         uint256 price;
+        string tokenurl;
         bool isListed;
     }
 
-    mapping(uint256 => NFT) private items;
-    mapping(address => NFT[] ) private itemsbyaddress;
+    mapping(uint256 => NFT) public items;
+    mapping(address => uint256[]) private itemsbyaddress; 
  
     uint256[] items_listed; 
 
-    function addNFT(string memory label, uint256 price) public notContractOwner {
+    function addNFT(uint256 price, string memory name, string memory tokenUrl) public notContractOwner {
         totalNFTs++;
         uint256 newItemId = totalNFTs;
-        //_mint(owner, newItemId);
-        //_setTokenURI(newItemId, label);
-        //create token here?
         
         NFT memory newNFT = NFT(
-            totalNFTs, label, msg.sender, price, false
+            newItemId, name, msg.sender, price, tokenUrl, false
         );
         
         items[newItemId] = newNFT;
-        //add to the itemsbyaddress
-        itemsbyaddress[msg.sender].push(newNFT);
-       
-
+        itemsbyaddress[msg.sender].push(newItemId);
     }
 
-    function removeFromList( uint256 id, address user ) private {
-        NFT[] storage itemslist = itemsbyaddress[user];
-        for ( uint256 i = 0; i < itemslist.length; i++ ){
-            if ( itemslist[i].id == id ) {
-                if ( itemslist.length > 1 ) {
-                    itemslist[i] = itemslist[itemslist.length-1]; //replace with last item in the list? hmm 
-                    //didnt want to have gap between them, any other idea how to do it? delete itemslist[i]?
-                }
-                itemslist.pop();              
-                break;
-            }
-        }
-        itemsbyaddress[user] = itemslist;
+    function removeFromList( uint256 id, address user ) private onlyNFTOwner(id) {
+        delete itemsbyaddress[user][id]; //joyce and zach: remove from list
     }
 
-    function buyNFT( uint256 id ) public payable notContractOwner {
-        //require( msg.sender != items[id].currentOwner); //buyer and seller not the same person, if i'm not mistaken, opensea allows user to buy their own nft, i leave it to u
-        require( msg.value >= items[id].price );
+    // function buyNFT( uint256 id ) public payable notContractOwner {
+    //     require( msg.value >= items[id].price );
 
         
-        address payable sendTo = payable(items[id].currentOwner);
-        // send token's worth of ethers to the owner
-        sendTo.transfer(msg.value);
-        
-        //_transfer( sendTo, msg.sender, id ); //xfer token ownership
-        
-        //remove id from itemsbyaddress
-        removeFromList(id, items[id].currentOwner);
+    //     address payable sendTo = payable(items[id].currentOwner);
+    //     // send token's worth of ethers to the owner
+    //     sendTo.transfer(msg.value);
 
-        items[id].currentOwner = msg.sender;//update to new owner of the nft
-        itemsbyaddress[msg.sender].push( items[id] );
-    }
+    //     removeFromList(id, items[id].currentOwner);
+
+    //     //update to new owner of the nft
+    //     items[id].currentOwner = msg.sender;
+    //     itemsbyaddress[msg.sender].push( id );
+    // }
     
-    //To list the NFT on the marketplace
-    // listing nft does perform any transactions
     function listNFT(uint256 id, uint256 price) public onlyNFTOwner(id) {
-        //require(msg.value == 0);
         require(price >= 0);
         items[id].isListed = true;
         items[id].price = price;
     }
 
-    // delisting does not require any transactions to take place
     function delistNFT(uint256 id)public onlyNFTOwner(id) {
-        //require(items[id].isListed == true, "Item is not listed"); //not required
-        //require(msg.value == 0);
         items[id].isListed = false;
     }
 
-    // return entire nft list, forget to mention listed nfts only
-    function getNFTList() public view returns ( NFT[] memory ) {
-        // instead of doing all these work to process the data, it's better to just store the id in the struct on creation
-        // how to return mapping without using for loop?
-        NFT[] memory nfts = new NFT[](totalNFTs+1);
-        uint count; 
-        for ( uint i=0; i < totalNFTs+1; i++)
+    function getUserNFTList(address user) public view returns (NFT[] memory) {
+        uint256[] memory useritems = itemsbyaddress[user];
+        NFT[] memory nfts = new NFT[](useritems.length+1);
+        
+        for ( uint i=0; i < useritems.length; i++)
         {
             NFT storage nft = items[i];
-            if ( nft.isListed )
-                nfts[count] = nft;
-                count++;
+            nfts[i] = nft;
         }
-
         return (nfts);
-    }
-
-    function getUserNFTList() public view returns (NFT[] memory) {
-        // return the actual nft data not the id
-        return itemsbyaddress[msg.sender];
     }
 
     modifier notContractOwner() {
@@ -122,7 +84,6 @@ contract MarketPlace {
     }
 
     modifier onlyNFTOwner(uint256 id) {
-        //Make sure only owner of NFT can do this
         require(msg.sender == items[id].currentOwner);
         _;
     }
