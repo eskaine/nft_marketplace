a8bb3409f61fc8a5d01b72c3d9de187534f2ddc8
tatebytes <tatebytes@gmail.com>
2022-05-24 20:19:50 +0800

    removed section

--- client/src/components/pages/NFTDetail.jsx
+++ client/src/components/pages/NFTDetail.jsx
@@ -15,30 +15,6 @@ function NFTDetail() {
           <div className="single__nft__content">
             <h2>{nft.label}</h2>
 
-            <div className=" d-flex align-items-center justify-content-between mt-4 mb-4">
-              <div className=" d-flex align-items-center gap-4 single__nft-seen">
-                <span>
-                  <i className="ri-eye-line" />
-                  {' '}
-                  234
-                </span>
-                <span>
-                  <i className="ri-heart-line" />
-                  {' '}
-                  123
-                </span>
-              </div>
-
-              <div className=" d-flex align-items-center gap-2 single__nft-more">
-                <span>
-                  <i className="ri-send-plane-line" />
-                </span>
-                <span>
-                  <i className="ri-more-2-line" />
-                </span>
-              </div>
-            </div>
-
             <div className="nft__creator d-flex gap-3 align-items-center">
               <div className="creator__img">
                 <img src={nft.imageUrl} alt="" className="w-100" />
