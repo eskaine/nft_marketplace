a5673f21145b19f2ff47411ef5a62ae8077e8a13
tatebytes <tatebytes@gmail.com>
2022-05-24 17:10:07 +0800

    Fixed compilation issues

--- client/src/components/pages/CreateNFT.jsx
+++ client/src/components/pages/CreateNFT.jsx
@@ -1,68 +1,48 @@
-import { ethers } from 'ethers';
-import React, { useContext, useState } from 'react';
-import { EthersContext } from '../utils/EthersProvider';
-import NFTDisplay from './shared/NFTDisplay';
-import "../styles/base.css";
+import React, { useContext } from 'react';
+import { EthersContext } from '../../utils/EthersProvider';
+import NFTDisplay from '../shared/NFTDisplay';
 
 function CreateNFT() {
-	const { getNFTList } = useContext(EthersContext);
+  const { getNFTList } = useContext(EthersContext);
   return (
     <main className="container mx-auto mt-10">
-    <>
       <section>
-        <Container>
-          <Row>
-            <Col lg="3" md="4" sm="6">
-              <h5 className="mb-4 text-light">Preview Item</h5>
-			  {getNFTList().map((nft) => (
-				<NFTDisplay
-					label={nft.label}
-					imageUrl={nft.imageUrl}
-					owner={nft.owner}
-					price={nft.price}
-				/>
-				))}
-            </Col>
-
-            <Col lg="9" md="8" sm="6">
-              <div className="create__item">
-                <form>
-                  <div className="form__input">
-                    <label htmlFor="">Upload File</label>
-                    <input type="file" className="upload__input" />
-                  </div>
-
-                  <div className="form__input">
-                    <label htmlFor="">Price</label>
-                    <input
-                      type="number"
-                      placeholder="Enter price for one item (ETH)"
-                    />
-                  </div>
-
-                  <div className=" d-flex align-items-center gap-4">
-                    <div className="form__input w-50">
-                      <label htmlFor="">Starting Date</label>
-                      <input type="date" />
-                    </div>
-
-                    <div className="form__input w-50">
-                      <label htmlFor="">Expiration Date</label>
-                      <input type="date" />
-                    </div>
-                  </div>
-
-                  <div className="form__input">
-                    <label htmlFor="">Title</label>
-                    <input type="text" placeholder="Enter title" />
-                  </div>
-                </form>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+        <h5 className="mb-4 text-light">Preview Item</h5>
+        {getNFTList().map((nft) => (
+          <NFTDisplay
+            label={nft.label}
+            imageUrl={nft.imageUrl}
+            owner={nft.owner}
+            price={nft.price}
+          />
+        ))}
+        <div className="create__item">
+          <form>
+            <div className="form__input">
+              <label htmlFor="uploadfile">Upload File</label>
+              <input id="uploadfile" type="file" className="upload__input" />
+            </div>
+            <div className="form__input">
+              <label htmlFor="price">Price</label>
+              <input
+                id="uploadfile"
+                type="number"
+                placeholder="Enter price for one item (ETH)"
+              />
+            </div>
+            <div className="form__input">
+              <label htmlFor="title">Title</label>
+              <input id="title" type="text" placeholder="Enter title" />
+            </div>
+            <div>
+              <label htmlFor="listedTick">
+                <input id="listedTick" type="checkbox" />
+                To be Listed
+              </label>
+            </div>
+          </form>
+        </div>
       </section>
-    </>
     </main>
   );
 }
--- client/src/components/pages/NFTDetail.jsx
+++ client/src/components/pages/NFTDetail.jsx
@@ -1,79 +1,63 @@
-import "../styles/base.css";
-import { Link  } from "react-router-dom";
-import { Container, Row, Col } from "reactstrap";
-import { EthersContext } from '../utils/EthersProvider';
-import NFTDisplay from './shared/NFTDisplay';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { EthersContext } from '../../utils/EthersProvider';
 
 function NFTDetail() {
-	const { getNFTList } = useContext(EthersContext);
+  const { getNFTList } = useContext(EthersContext);
   return (
-	<>
-		<div className="flex justify-center flex-wrap">
-	{getNFTList().map((nft) => (
-	  <NFTDisplay
-		label={nft.label}
-		imageUrl={nft.imageUrl}
-		owner={nft.owner}
-		price={nft.price}
-	  />
-	))}
-  </div>
-	<section>
-	  <Container>
-		<Row>
-		  <Col lg="6" md="6" sm="6">
-			<img
-			  src={nft.imageUrl}
-			  alt=""
-			  className="w-100 single__nft-img"
-			/>
-		  </Col>
+    <div className="flex justify-center flex-wrap">
+      {getNFTList().map((nft) => (
+        <div className="flex justify-center flex-wrap">
+          <img
+            src={nft.imageUrl}
+            alt=""
+            className="w-100 single__nft-img"
+          />
+          <div className="single__nft__content">
+            <h2>{nft.label}</h2>
 
-		  <Col lg="6" md="6" sm="6">
-			<div className="single__nft__content">
-			  <h2>{nft.label}</h2>
+            <div className=" d-flex align-items-center justify-content-between mt-4 mb-4">
+              <div className=" d-flex align-items-center gap-4 single__nft-seen">
+                <span>
+                  <i className="ri-eye-line" />
+                  {' '}
+                  234
+                </span>
+                <span>
+                  <i className="ri-heart-line" />
+                  {' '}
+                  123
+                </span>
+              </div>
 
-			  <div className=" d-flex align-items-center justify-content-between mt-4 mb-4">
-				<div className=" d-flex align-items-center gap-4 single__nft-seen">
-				  <span>
-					<i class="ri-eye-line"></i> 234
-				  </span>
-				  <span>
-					<i class="ri-heart-line"></i> 123
-				  </span>
-				</div>
+              <div className=" d-flex align-items-center gap-2 single__nft-more">
+                <span>
+                  <i className="ri-send-plane-line" />
+                </span>
+                <span>
+                  <i className="ri-more-2-line" />
+                </span>
+              </div>
+            </div>
 
-				<div className=" d-flex align-items-center gap-2 single__nft-more">
-				  <span>
-					<i class="ri-send-plane-line"></i>
-				  </span>
-				  <span>
-					<i class="ri-more-2-line"></i>
-				  </span>
-				</div>
-			  </div>
+            <div className="nft__creator d-flex gap-3 align-items-center">
+              <div className="creator__img">
+                <img src={nft.imageUrl} alt="" className="w-100" />
+              </div>
 
-			  <div className="nft__creator d-flex gap-3 align-items-center">
-				<div className="creator__img">
-				  <img src={nft.imageUrl} alt="" className="w-100" />
-				</div>
-
-				<div className="creator__detail">
-				  <p>Created By</p>
-				  <h6>{nft.owner}</h6>
-				</div>
-			  </div>
-			  <button className="singleNft-btn d-flex align-items-center gap-2 w-100">
-				<i class="ri-shopping-bag-line"></i>
-				<Link to="/wallet">Buy Now</Link>
-			  </button>
-			</div>
-		  </Col>
-		</Row>
-	  </Container>
-	</section>
-
-  </>
+              <div className="creator__detail">
+                <p>Created By</p>
+                <h6>{nft.owner}</h6>
+              </div>
+            </div>
+            <button className="singleNft-btn d-flex align-items-center gap-2 w-100">
+              <i className="ri-shopping-bag-line" />
+              <Link to="/wallet">Buy Now</Link>
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
 
