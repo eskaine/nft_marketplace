4282920d955812f4761669d19cbd73158ea6daa7
tatebytes <tatebytes@gmail.com>
2022-05-24 17:58:33 +0800

    fixed compile issue

--- client/src/components/pages/CreateNFT.jsx
+++ client/src/components/pages/CreateNFT.jsx
@@ -19,20 +19,26 @@ function CreateNFT() {
         <div className="create__item">
           <form>
             <div className="form__input">
-              <label htmlFor="uploadfile">Upload File</label>
-              <input id="uploadfile" type="file" className="upload__input" />
+              <label htmlFor="uploadfile">
+                Upload File
+                <input id="uploadfile" type="file" className="upload__input" />
+              </label>
             </div>
             <div className="form__input">
-              <label htmlFor="price">Price</label>
-              <input
-                id="uploadfile"
-                type="number"
-                placeholder="Enter price for one item (ETH)"
-              />
+              <label htmlFor="price">
+                Price
+                <input
+                  id="uploadfile"
+                  type="number"
+                  placeholder="Enter price for one item (ETH)"
+                />
+              </label>
             </div>
             <div className="form__input">
-              <label htmlFor="title">Title</label>
-              <input id="title" type="text" placeholder="Enter title" />
+              <label htmlFor="title">
+                Title
+                <input id="title" type="text" placeholder="Enter title" />
+              </label>
             </div>
             <div>
               <label htmlFor="listedTick">
